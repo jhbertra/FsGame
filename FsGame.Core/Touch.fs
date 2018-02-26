e1723c5e39a97e5b1ee5d457c97ded2ad6d18de8
Jamie Bertram <jamie@novoideation.ca>
2018-02-25 21:04:20 -0700

    Add touch and iOS

---
+++ FsGame.Core/Touch.fs
@@ -0,0 +1,151 @@
+﻿namespace FsGame
+
+open Core
+open FsEssentials.Prelude
+
+module Touch =
+
+    //
+    // --------- Types ---------
+    //
+
+    type Touch = Touch of int * Point
+
+
+    type GestureSample = GestureSample of GameTime * Touch
+
+
+    type TouchEventType =
+        | TouchDown of Point
+        | TouchMoved of Point
+        | TouchUp of Point
+
+
+    type TouchEvent = int * GameTime * TouchEventType
+
+
+    type PendingGestureType =
+        | Tap of GameTime * Point
+        | Drag of Point
+
+
+    type PendingGesture = int * PendingGestureType
+
+
+    type GestureType =
+        | TouchDown of Point
+        | TouchUp of Point
+        | Tap of Point
+        | Drag of Point * Delta
+
+
+    type Gesture = int * GestureType
+
+
+    type EventProcessResult =
+        | PendingGesture of PendingGesture
+        | Gesture of Gesture
+
+
+
+
+    //
+    // --------- Functions ---------
+    //
+
+
+    let dragThreshold = 5.0
+
+
+    let gestureSample gameTime touch  = GestureSample ( gameTime , touch )
+
+
+    let id ( Touch ( id , _ ) ) = id
+
+
+    let position ( Touch ( _ , pos ) ) = pos
+
+
+    let touchEvent gameTime = function
+        | ( Some ( Touch _ ) ),( Some ( Touch ( id , pos ) ) ) ->
+            [ id, gameTime, TouchEventType.TouchMoved pos ]
+
+        | (Some (Touch (id,pos))),None ->
+            [ id, gameTime, TouchEventType.TouchUp pos ]
+
+        | None,(Some (Touch (id,pos))) ->
+            [ id, gameTime, TouchEventType.TouchDown pos ]
+
+        | _ -> []
+
+
+
+    let touchEvents previousTouches newTouches gameTime =
+        let length = List.length newTouches
+        fullJoin previousTouches newTouches id id
+        |> List.collect (touchEvent gameTime)
+
+
+    let processEvent = function
+        | _,(Some (id,gameTime,TouchEventType.TouchDown position)) -> 
+                [
+                Gesture(id,TouchDown position)
+                PendingGesture(id,PendingGestureType.Tap (gameTime,position))
+                ] 
+
+        | (Some (_,(PendingGestureType.Tap (tapStartTime, startPos)))),(Some (id,time,TouchEventType.TouchUp endPos)) 
+            when time.total.Subtract(tapStartTime.total).Milliseconds < 500 && distance startPos endPos < dragThreshold ->
+                [
+                Gesture(id,TouchUp endPos)
+                Gesture(id,Tap endPos)
+                ]
+
+        | (Some (_,(PendingGestureType.Tap (tapStartTime, startPos)))),(Some (id,time,TouchMoved endPos)) 
+            when time.total.Subtract(tapStartTime.total).Milliseconds < 500 && distance startPos endPos < dragThreshold ->
+                [PendingGesture(id,PendingGestureType.Tap (tapStartTime,startPos))]
+
+        | (Some (_,(PendingGestureType.Tap (tapStartTime, startPos)))),(Some (id,time,TouchMoved endPos)) 
+            when time.total.Subtract(tapStartTime.total).Milliseconds >= 500 && distance startPos endPos < dragThreshold ->
+                [PendingGesture(id,PendingGestureType.Drag (startPos))]
+
+        | (Some (_,(PendingGestureType.Tap (_, startPos)))),(Some (id,_,TouchEventType.TouchUp endPos)) 
+            when distance startPos endPos >= dragThreshold ->
+                [
+                Gesture(id,TouchUp endPos)
+                Gesture(id,Drag (startPos,(subtract startPos endPos)))
+                ]
+
+        | (Some (_,(PendingGestureType.Tap (_, startPos)))),(Some (id,_,TouchMoved endPos)) 
+            when distance startPos endPos >= dragThreshold ->
+                [
+                Gesture(id,Drag (startPos,(subtract startPos endPos)))
+                PendingGesture(id, PendingGestureType.Drag (endPos))
+                ]
+
+        | (Some (_,(PendingGestureType.Drag startPos))),(Some (id,_,TouchMoved endPos)) ->
+            [
+            Gesture(id,Drag (startPos,(subtract startPos endPos)))
+            PendingGesture(id, PendingGestureType.Drag (endPos))
+            ]
+
+        | (Some (_,(PendingGestureType.Drag startPos))),(Some (id,_,TouchEventType.TouchUp endPos)) ->
+            [
+            Gesture(id,Drag (startPos,(subtract startPos endPos)))
+            Gesture(id,TouchUp endPos)
+            PendingGesture(id, PendingGestureType.Drag (endPos))
+            ]
+
+        | _,(Some (id,_,TouchEventType.TouchUp pos)) -> [Gesture(id,TouchUp pos)]
+
+        | _ -> [] 
+
+
+    let processEvents (previousPending : PendingGesture list) (events : TouchEvent list) = 
+        fullJoin previousPending events fst (fun (id,_,_) -> id) 
+        |> List.collect processEvent
+
+
+    let gestures = List.collect (function Gesture x -> [x] | _ -> [])
+
+
+    let pendingGestures = List.collect (function PendingGesture x -> [x] | _ -> [])
---
+++ FsGame.iOS/Platform.fs
@@ -0,0 +1,185 @@
+﻿namespace FsGame
+
+open FsEssentials
+open Prelude
+
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Audio
+open Microsoft.Xna.Framework.Input.Touch
+
+open Core
+open Touch
+
+module iOS =
+
+    let vector2ToFloatTuple (vec : Microsoft.Xna.Framework.Vector2) = ((float)vec.X,(float)vec.Y)
+        
+    let scale = 750.0 / float GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width // temporary hard-coded scaling
+
+    let gameSpace = (*) scale
+
+    let screenSpace x = x / scale
+
+    let getTouches (touchCol: TouchCollection) =
+        touchCol
+        |> List.ofSeq
+        |> List.map (fun touch -> 
+            Touch (touch.Id,touch.Position |> vector2ToFloatTuple |> mapT2 gameSpace |> Point ))
+
+    type TouchGame<'m, 't>(engine : GameEngine<'m, Touch list, 't>) as this =
+        inherit Microsoft.Xna.Framework.Game()
+
+
+        let _ = new Microsoft.Xna.Framework.GraphicsDeviceManager(this)
+
+
+        [<DefaultValue>] val mutable model : 'm
+        [<DefaultValue>] val mutable delayed : ( System.TimeSpan * float * Update<'m, Touch list, 't> ) list
+        [<DefaultValue>] val mutable objects : GameObject<'t> list
+        [<DefaultValue>] val mutable textures : Map<string, Texture2D>
+        [<DefaultValue>] val mutable sfxInstances : Map<string, SoundEffectInstance>
+        [<DefaultValue>] val mutable sfx : Map<string, SoundEffect>
+        [<DefaultValue>] val mutable spriteBatch : SpriteBatch
+
+
+        let rec runCommands totalTime = function
+            | [] -> ()
+            | PlaySound (sound, mode, volume)::xs ->
+                Option.maybe {
+                    let! sfx = this.sfxInstances.TryFind sound
+                    if sfx.State = SoundState.Stopped then
+                        sfx.Volume <- float32 volume
+                        sfx.Play() |> ignore
+                    else
+                        match mode with
+                        | Overlap -> 
+                                let! sfx = this.sfx.TryFind sound
+                                let instance = sfx.CreateInstance()
+                                instance.Volume <- float32 volume
+                                instance.Play() |> ignore
+                        | NoOverlap -> () |> ignore
+                } |> ignore
+                runCommands totalTime xs
+            | Delay ( delay , update ) :: xs ->
+                this.delayed <- List.rev ( ( totalTime , delay , update ) :: List.rev this.delayed )
+                runCommands totalTime xs
+
+
+
+        //
+        // --------- Initialize ---------
+        //
+
+        override this.Initialize() = 
+            this.spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+            this.model <- engine.init
+            this.delayed <- []
+            TouchPanel.EnabledGestures <- Microsoft.Xna.Framework.Input.Touch.GestureType.Tap ||| GestureType.FreeDrag ||| GestureType.DragComplete
+            base.Initialize()
+
+
+
+        //
+        // --------- Load ---------
+        //
+
+        let findTexture t = Map.find t this.textures
+
+
+        let rec makeBox (findTexture : string -> Texture2D) (Point (x,y)) = function
+            | [] -> BoundingBox (x, y, 0.0, 0.0)
+            | t::ts ->
+                let texture = findTexture t
+                let tBox = BoundingBox (x, y, float texture.Width, float texture.Height)
+                makeBox findTexture (Point (x,y)) ts |> union tBox
+
+
+        let toGameObject findTexture = function
+            | Area (texture, point, tag) -> { textures = []; box = makeBox findTexture point [texture]; alpha = 1.0; tag = tag }
+            | Sprite (textures, point, alpha, tag) -> { textures = textures; box = makeBox findTexture point textures; alpha = alpha; tag = tag }
+            
+
+        override this.LoadContent() =
+            let manifest = engine.contentManifest
+            this.Content.RootDirectory <- manifest.root
+            this.textures <- 
+                manifest.textures
+                |> List.fold (fun m t -> Map.add t (this.Content.Load<Texture2D>("textures/" + t)) m) (Map[])
+            let sfxList = manifest.sfx |> List.map (fun s -> (s,this.Content.Load<SoundEffect>("sfx/" + s)))
+            this.sfxInstances <- 
+                sfxList
+                |> List.fold (fun m (s,fx) -> Map.add s (fx.CreateInstance()) m) (Map[])
+            this.sfx <- 
+                sfxList
+                |> List.fold (fun m (s,fx) -> Map.add s (fx) m) (Map[])
+            this.objects <- engine.draw this.model |> List.map (toGameObject findTexture)
+            base.LoadContent()
+
+
+
+        //
+        // --------- Update ---------
+        //
+
+
+        let rec runDelayed gameState = function
+        | [] -> State.fromValue gameState.model
+        | update :: updates ->
+                let (UpdateResult (model,newCommands)) = update gameState
+                State.modify (newCommands |> flip (@))
+                |> State.bind ( fun () -> runDelayed { gameState with model = model } updates )
+
+
+        override this.Update(gameTime) =
+            let touches = getTouches (TouchPanel.GetState())
+
+            let gameState = { 
+                controller = touches
+                gameTime = 
+                    { 
+                    elapsed = gameTime.ElapsedGameTime
+                    total = gameTime.TotalGameTime
+                    isRunningSlowly = gameTime.IsRunningSlowly 
+                    }
+                objects = this.objects
+                model = this.model 
+                }
+
+            let elapsedDelays = 
+                this.delayed
+                |> List.takeWhile ( fun ( start : System.TimeSpan , wait , _ ) ->
+                    (gameTime.TotalGameTime.TotalMilliseconds - start.TotalMilliseconds) * 0.001 >= wait )
+                |> List.map ( fun ( _ , _ , update ) -> update )
+
+            let ( commands , model ) = 
+                State.state {
+                    let! model = runDelayed gameState elapsedDelays
+                    let ( UpdateResult ( model , newCommands ) ) = engine.update gameState
+                    do! State.modify (newCommands |> flip (@))
+                    return model
+                }
+                |> State.run []
+
+            
+            this.delayed <- List.skip ( List.length elapsedDelays ) this.delayed
+
+            this.model <- model
+            runCommands gameTime.TotalGameTime commands                
+            base.Update(gameTime)
+
+
+
+        //
+        // --------- Draw ---------
+        //
+
+        override this.Draw gameTime = 
+            this.objects <- engine.draw this.model |> List.map (toGameObject findTexture)
+            this.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend)
+            for object in this.objects do
+                for texture in object.textures do
+                    let texture2D = findTexture texture
+                    let x,y,w,h = bounds object |> mapT4 screenSpace |> mapT4 int
+                    this.spriteBatch.Draw(texture2D, Microsoft.Xna.Framework.Rectangle(x, y, w, h), Microsoft.Xna.Framework.Color.White * float32 object.alpha)
+            this.spriteBatch.End()
+            base.Draw(gameTime)
